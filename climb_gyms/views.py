5bbc3283f9a834f9709f5871d41cb0ad9feb2730
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-07-22 13:37:54 +0200

    Add search functionality to climbing gyms view

--- climb_gyms/views.py
+++ climb_gyms/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView
 
+from django.db.models import Q
+
 from .models import ClimbingGyms
 
 class ClimbingGymsView(ListView):
@@ -8,3 +10,23 @@ class ClimbingGymsView(ListView):
     model = ClimbingGyms
     context_object_name = 'climbing_gyms'
     paginate_by = 5
+
+    def get_queryset(self, **kwargs):
+        """ Get queryset """
+        query = self.request.GET.get('q')
+        button_query = self.request.GET.get('button_query')
+
+        if query:
+            climbing_gyms = self.model.objects.filter(
+                Q(description__icontains=query) |
+                Q(city__icontains=query)
+            )
+        elif button_query:
+            climbing_gyms = self.model.objects.filter(
+                Q(city__icontains=button_query)
+            )
+
+        else:
+            climbing_gyms = self.model.objects.all()
+
+        return climbing_gyms 
