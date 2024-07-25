9d9eec41bafcf13e626c3d82801084a0febe2570
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-07-26 00:42:16 +0200

    Update profile model and migrations

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from djrichtextfield.models import RichTextField
 
 from django.contrib.auth.models import User
+from climb_gyms.models import Comments
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -10,8 +11,8 @@ from django_resized import ResizedImageField
 class Profile(models.Model):
     """" Model for user profile """
     TYPE_OF_CLIMBS = (
-        (0, 'Boulder'), (1, 'Lead'),
-        (2, 'Speed'), (3, 'Outdoors')
+        ('Boulder', 'boulder'), ('Lead', 'lead'),
+        ('Speed', 'speed'), ('Outdoors', 'outdoors')
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     bio = RichTextField(max_length=2000, blank=True, null=True)
@@ -22,8 +23,8 @@ class Profile(models.Model):
         force_format="WEBP", 
         blank=True, null=True
     )
-    fav_climb = models.IntegerField(choices=TYPE_OF_CLIMBS, default=0)
-    #comments = models.ForeigeKey(Comments, on_delete=models.CASCADE, blank=True, null=True)
+    fav_climb = models.CharField(choices=TYPE_OF_CLIMBS, default='null')
+    comments = models.ForeignKey(Comments, on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
