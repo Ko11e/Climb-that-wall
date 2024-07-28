2d920acbdabd062a36974128022d7357868ccb06
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-07-28 23:42:26 +0200

    Add delete profile functionality

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ProfileView, EditProfileView
+from .views import ProfileView, EditProfileView, DeleteProfileView, ConfirmDeleteProfileView
 
 urlpatterns = [
     path('user/<slug:pk>/', ProfileView.as_view(), name='profile'),
     path('edit/<slug:pk>/', EditProfileView.as_view(), name='edit_profile'),
+    path('delete/<slug:pk>/', DeleteProfileView.as_view(), name='delete_profile'),
+    path('delete/<slug:pk>/confirm/', ConfirmDeleteProfileView.as_view(), name='delete_profile_confirm'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,13 @@
-from django.views.generic import TemplateView, UpdateView
+from django.views.generic import TemplateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
-from django.shortcuts import get_object_or_404
 
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+from django.contrib.auth.models import User
 from .models import Profile
+
 from .forms import ProfileForm
 
 class ProfileView(TemplateView):
@@ -33,5 +38,41 @@ class EditProfileView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         return self.request.user == self.get_object().user
 
+class ConfirmDeleteProfileView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    """ Confirm delete user profile view """
+    template_name = 'profiles/delete_profile_confirm.html'
+
+    def get_context_data(self, **kwargs):
+        profile = Profile.objects.get(user=self.kwargs['pk'])
+        context = {
+            'profile': profile
+        }
+
+        return context
     
+    def test_func(self):
+        return self.request.user == Profile.objects.get(user=self.kwargs['pk']).user
+
+
+class DeleteProfileView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """ Delete user profile view """
+    model = User
+    template_name = 'profiles/profile_delete.html'
+    success_url = '/'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(User, user__id=self.kwargs['pk'])
+
+
+    def form_valid(self, form):
+        # Perform the deletion
+        super().form_valid(form)
+        # Log the user out
+        logout(self.request)
+        # Redirect to the success URL
+        return redirect(self.get_success_url())
+    
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
 
