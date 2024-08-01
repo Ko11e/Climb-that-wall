d62640a001754b9f92983906812a071e907d8a54
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-08-01 21:59:42 +0200

    Add url path to edit comment and add link to profile page

--- climb_gyms/urls.py
+++ climb_gyms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 # imported the views
-from .views import ClimbingGymsView, ClimbGymView, CreateCommentsView
+from .views import ClimbingGymsView, ClimbGymView, CreateCommentsView, EditCommentsView
 
 urlpatterns = [
     path("search/", ClimbingGymsView.as_view(), name="search_gyms"),
     path("<slug:slug>/", ClimbGymView.as_view(), name="gyms"),
     path("comment/<int:pk>", CreateCommentsView.as_view() , name="create_comment"),
+    path("comment/edit/<int:pk>", EditCommentsView.as_view(), name="edit_comment"),
 ]
