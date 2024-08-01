1034dc301967cf6cef812cbdc68309e54209c69f
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-08-01 16:19:12 +0200

    Update profile model and migrations

--- profiles/models.py
+++ profiles/models.py
@@ -8,31 +8,38 @@ from django.dispatch import receiver
 
 from django_resized import ResizedImageField
 
+
 class Profile(models.Model):
-    """" Model for user profile """
+    """ " Model for user profile"""
+
     TYPE_OF_CLIMBS = (
-        ('Boulder', 'boulder'), ('Lead', 'lead'),
-        ('Speed', 'speed'), ('Outdoors', 'outdoors')
+        ("Boulder", "boulder"),
+        ("Lead", "lead"),
+        ("Speed", "speed"),
+        ("Outdoors", "outdoors"),
     )
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
     bio = RichTextField(max_length=2000, blank=True, null=True)
     profile_pic = ResizedImageField(
-        size=[300, 300], 
-        quality=75, 
-        upload_to='profile_pics/',
-        force_format="WEBP", 
-        blank=True, null=True
+        size=[300, 300],
+        quality=75,
+        upload_to="profile_pics/",
+        force_format="WEBP",
+        blank=True,
+        null=True,
     )
-    fav_climb = models.CharField(choices=TYPE_OF_CLIMBS, default='null')
-    comments = models.ForeignKey(Comments, on_delete=models.CASCADE, blank=True, null=True)
+    fav_climb = models.CharField(choices=TYPE_OF_CLIMBS, default="null")
+    comments = models.ForeignKey(
+        Comments, on_delete=models.CASCADE, blank=True, null=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
-
 
     def __str__(self):
         return self.user.username
 
+
 @receiver(post_save, sender=User)
 def create_user_profile(instance, created, **kwargs):
-    """ Create a user profile when a new user is created """
+    """Create a user profile when a new user is created"""
     if created:
         Profile.objects.create(user=instance)
