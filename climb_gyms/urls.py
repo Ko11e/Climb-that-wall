0964401d51b3fa438ae0b00f592c34eaef5b4819
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-08-03 01:16:55 +0200

    Add url path for delete comment in gym.html and profile.html templates

--- climb_gyms/urls.py
+++ climb_gyms/urls.py
@@ -11,22 +11,25 @@ from .views import (
     DeleteClimbingGymView,
     CreateCommentsView,
     EditCommentsView,
+    DeleteCommentsView,
     create_climbing_gym,
     not_authorized_view,
 )
 
 urlpatterns = [
     path("search/", ClimbingGymsView.as_view(), name="search_gyms"),
-
     # Climbing gyms detail view
     path("<slug:slug>/", ClimbGymView.as_view(), name="gyms"),
     path("create-climbing-gym/", create_climbing_gym, name="create_climbing_gym"),
-    
     # Edit climbing gym
     path("edit-gym/<int:pk>", EditClimbingGymView.as_view(), name="edit_climbing_gym"),
-    path("edit-gym-text/<int:pk>", EditGymTextView.as_view(), name="edit_gym_text"),
     path(
-        "edit-gym-images/<int:pk>", EditGymImagesView.as_view(), name="edit_gym_images"
+        "edit-gym-text/<int:pk>", EditGymTextView.as_view(),
+        name="edit_gym_text"
+    ),
+    path(
+        "edit-gym-images/<int:pk>", EditGymImagesView.as_view(),
+        name="edit_gym_images"
     ),
     path(
         "edit-gym-socialmedia/<int:pk>",
@@ -38,10 +41,21 @@ urlpatterns = [
         DeleteClimbingGymView.as_view(),
         name="delete_climbing_gym",
     ),
-
+    
     # Comments
-    path("comment/<int:pk>", CreateCommentsView.as_view(), name="create_comment"),
-    path("comment/edit/<int:pk>", EditCommentsView.as_view(), name="edit_comment"),
+    path(
+        "comment/<int:pk>", CreateCommentsView.as_view(),
+        name="create_comment"
+    ),
+    path(
+        "comment/edit/<int:pk>", EditCommentsView.as_view(),
+        name="edit_comment"
+    ),
+    path(
+        "comment/delete/<int:pk>",
+        DeleteCommentsView.as_view(),
+        name="delete_comment"
+    ),
     path(
         "create-climbing-gym/not-authorized/",
         not_authorized_view,
