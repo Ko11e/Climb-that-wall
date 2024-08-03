0acb4bbe43817fda6d329da9646f396d97f91709
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-08-03 02:07:35 +0200

    Add ContactUsForm for user contact information

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Profile
+from .models import Profile, ContactUs
 
 class ProfileForm(forms.ModelForm):
     """ Form for user profile """
@@ -12,4 +12,25 @@ class ProfileForm(forms.ModelForm):
             'profile_pic': 'Avatar',
             'bio': 'Bio',
             'fav_climb': 'Favorite Climbing type'
+        }
+
+class ContactUsForm(forms.ModelForm):
+    """ Form for user contact information """
+    
+    class Meta:
+        model = ContactUs
+        fields = ['name',  'question', 'email', 'message']
+
+        labels = {
+            'name': 'Name (First and Last)',
+            'question': 'What is your question about?',
+            'email': 'Email',
+            'message': 'Your message'
+        }
+
+        widget = {
+            'message': forms.Textarea(attrs={'rows': 4}),
+            'question': forms.Select(choices=ContactUs.QUESTION),
+            'name': forms.TextInput(attrs={'placeholder': 'First and Last Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'ex. first.lastname@mailadress.com'})
         }
