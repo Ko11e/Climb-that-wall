4a805af89a88265f7d3e5e45524c0a568d3c1632
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-08-03 19:15:17 +0200

    Change order in the urls climbing gyms

--- climb_gyms/urls.py
+++ climb_gyms/urls.py
@@ -18,47 +18,15 @@ from .views import (
 
 urlpatterns = [
     path("search/", ClimbingGymsView.as_view(), name="search_gyms"),
-    # Climbing gyms detail view
-    path("<slug:slug>/", ClimbGymView.as_view(), name="gyms"),
     path("create-climbing-gym/", create_climbing_gym, name="create_climbing_gym"),
-    # Edit climbing gym
+    path("create-climbing-gym/not-authorized/", not_authorized_view, name="not_authorized"),
     path("edit-gym/<int:pk>", EditClimbingGymView.as_view(), name="edit_climbing_gym"),
-    path(
-        "edit-gym-text/<int:pk>", EditGymTextView.as_view(),
-        name="edit_gym_text"
-    ),
-    path(
-        "edit-gym-images/<int:pk>", EditGymImagesView.as_view(),
-        name="edit_gym_images"
-    ),
-    path(
-        "edit-gym-socialmedia/<int:pk>",
-        EditSocialmediaView.as_view(),
-        name="edit_gym_socialmedia",
-    ),
-    path(
-        "delete-gym/<int:pk>",
-        DeleteClimbingGymView.as_view(),
-        name="delete_climbing_gym",
-    ),
-    
-    # Comments
-    path(
-        "comment/<int:pk>", CreateCommentsView.as_view(),
-        name="create_comment"
-    ),
-    path(
-        "comment/edit/<int:pk>", EditCommentsView.as_view(),
-        name="edit_comment"
-    ),
-    path(
-        "comment/delete/<int:pk>",
-        DeleteCommentsView.as_view(),
-        name="delete_comment"
-    ),
-    path(
-        "create-climbing-gym/not-authorized/",
-        not_authorized_view,
-        name="not_authorized",
-    ),
+    path("edit-gym-text/<int:pk>", EditGymTextView.as_view(), name="edit_gym_text"),
+    path("edit-gym-images/<int:pk>", EditGymImagesView.as_view(), name="edit_gym_images"),
+    path("edit-gym-socialmedia/<int:pk>", EditSocialmediaView.as_view(), name="edit_gym_socialmedia"),
+    path("delete-gym/<int:pk>", DeleteClimbingGymView.as_view(), name="delete_climbing_gym"),
+    path("comment/<int:pk>", CreateCommentsView.as_view(), name="create_comment"),
+    path("comment/edit/<int:pk>", EditCommentsView.as_view(), name="edit_comment"),
+    path("comment/delete/<int:pk>", DeleteCommentsView.as_view(), name="delete_comment"),
+    path("<slug:slug>/", ClimbGymView.as_view(), name="gyms"),
 ]
