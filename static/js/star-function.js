f129e112a495d87ae5b2f25169ebd90c03758d47
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-08-06 13:28:16 +0200

    Make small styling changes

--- static/js/star-function.js
+++ static/js/star-function.js
@@ -1,11 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all labels and inputs
+    const inputBox = document.querySelector('.rating-box');
+    const labels = [...inputBox.querySelectorAll('label')];
+    const inputs = [...inputBox.querySelectorAll('input')];
+    
+    // Initialize to -1 to indicate no input is checked
+    let checkedInput = -1; 
+    
+    // Finds the checked value if it exist.
+    inputs.forEach((input, index) => {
+        if (input.checked) {
+            checkedInput = index;
+        }
+    });
+
+    if (checkedInput === -1) {
+        // No input is checked, reset all labels to default color
+        labels.forEach(lbl => lbl.style.color = '#888');
+    } else {
+        // Set the color based on the checked input
+        labels.forEach((lbl, i) => {
+            lbl.style.color = (i <= checkedInput) ? 'var(--yellow)' : '#888';
+            console.log(lbl.style.color);
+        });
+    }
+});
+
 function enableRatingModel(inputBox) {
     // Get all labels and inputs
     const labels = [...inputBox.querySelectorAll('label')];
     const inputs = [...inputBox.querySelectorAll('input')];
-    
+
     // Set click label to active and check the responsive input
     labels.forEach((e, index) => {
-        e.addEventListener('click', (event) => {
+        e.addEventListener('click', () => {
             labels[index].classList.add('active');
             setTimeout(() => {
                 labels[index].classList.remove('active');
@@ -15,7 +43,7 @@ function enableRatingModel(inputBox) {
     
     // Set hovereffect on the labels
     labels.forEach((e, index) => {
-        e.addEventListener('mouseover', (e) => {
+        e.addEventListener('mouseover', () => {
             for (let i = 0; i < labels.length; i++) {
                 labels[i].style.color = (i <= index) ? 'var(--yellow)' : '#888';
             }
@@ -38,6 +66,7 @@ function enableRatingModel(inputBox) {
     });
 
 }
+
 
 // Enable rating model
 if (document.querySelector('.rating-box'))
