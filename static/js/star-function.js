7a4c718217deaf27e658506703d9fa9db4295c3d
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-08-07 22:56:08 +0200

    Add if statmete if user is not login

--- static/js/star-function.js
+++ static/js/star-function.js
@@ -1,28 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get all labels and inputs
-    const inputBox = document.querySelector('.rating-box');
-    const labels = [...inputBox.querySelectorAll('label')];
-    const inputs = [...inputBox.querySelectorAll('input')];
-    
-    // Initialize to -1 to indicate no input is checked
-    let checkedInput = -1; 
-    
-    // Finds the checked value if it exist.
-    inputs.forEach((input, index) => {
-        if (input.checked) {
-            checkedInput = index;
-        }
-    });
-
-    if (checkedInput === -1) {
-        // No input is checked, reset all labels to default color
-        labels.forEach(lbl => lbl.style.color = '#888');
-    } else {
-        // Set the color based on the checked input
-        labels.forEach((lbl, i) => {
-            lbl.style.color = (i <= checkedInput) ? 'var(--yellow)' : '#888';
-            console.log(lbl.style.color);
+    if (document.querySelector('.rating-box')) {
+        // Get all labels and inputs
+        const inputBox = document.querySelector('.rating-box');
+        const labels = [...inputBox.querySelectorAll('label')];
+        const inputs = [...inputBox.querySelectorAll('input')];
+        
+        // Initialize to -1 to indicate no input is checked
+        let checkedInput = -1; 
+        
+        // Finds the checked value if it exist.
+        inputs.forEach((input, index) => {
+            if (input.checked) {
+                checkedInput = index;
+            }
         });
+
+        if (checkedInput === -1) {
+            // No input is checked, reset all labels to default color
+            labels.forEach(lbl => lbl.style.color = '#888');
+        } else {
+            // Set the color based on the checked input
+            labels.forEach((lbl, i) => {
+                lbl.style.color = (i <= checkedInput) ? 'var(--yellow)' : '#888';
+                console.log(lbl.style.color);
+            });
+        }
     }
 });
 
