165e8f0f8457a416223542f5421e12c20e7b2c7e
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-08-09 08:55:15 +0200

    Make last small feature changes to navbar

--- main/settings.py
+++ main/settings.py
@@ -129,6 +129,8 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com"]
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
